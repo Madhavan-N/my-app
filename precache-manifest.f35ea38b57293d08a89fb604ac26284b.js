5172fd977a4a5a273e20007be4f2b0079a09bb92
Madhavan-N <madhavannagarajan@outlook.com>
2020-01-09 19:27:40 +0530

    Updates

---
+++ precache-manifest.f35ea38b57293d08a89fb604ac26284b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baf12cdc08678e437997ef62fd312a75",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "af899f8a90434a1897a6",
+    "url": "/my-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a3e89b657e5f97d35ed1",
+    "url": "/my-app/static/js/2.310f69b6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-app/static/js/2.310f69b6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "af899f8a90434a1897a6",
+    "url": "/my-app/static/js/main.5c2a99af.chunk.js"
+  },
+  {
+    "revision": "0d6448e5de9302755c13",
+    "url": "/my-app/static/js/runtime-main.cedd286d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
